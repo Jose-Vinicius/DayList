82044e4891fa0129b5ee1c8536e27b0a7530c7e0
Jose vinicius francener sarmento <70924237+Jose-Vinicius@users.noreply.github.com>
2022-07-11 16:06:59 -0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC6j9tyX1q5XeMsFTIWWtvCb_vNP65cKuE",
+  authDomain: "daylist-819eb.firebaseapp.com",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App' 
+
+const Container = document.getElementById('root');
+const Root = ReactDOM.createRoot(Container);
+
+Root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
